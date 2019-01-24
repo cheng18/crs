256d8ec3ef6a014e44b87ac2736e0aef5fc385f3
HuangCheng <huangcheng@HuangChengde-MacBook-Air.local>
2019-01-24 23:45:41 +0800

    run1

--- bert/run1.sh
+++ bert/run1.sh
@@ -1,7 +1,20 @@
 
 export CUDA_VISIBLE_DEVICES=1
-export BERT_BASE_DIR=/crs2/bert/model/stroke_AA_vv_msl-256
+export BERT_BASE_DIR=/crs2/bert/model/chinese_AA_vv_msl-256
 export XNLI_DIR=/crs2/downstream_data/XNLI
+
+python3 create_pretraining_data.py \
+  --input_file=/crs2/pre-training_data/zhwiki-20181101-pages-articles/AA/* \
+  --output_file=$BERT_BASE_DIR/tmp/tf_zh_AA.tfrecord \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --do_lower_case=True \
+  --do_stroke=False \
+  --stroke_vocab_file=$BERT_BASE_DIR/stroke.csv \
+  --max_seq_length=256 \
+  --max_predictions_per_seq=40 \
+  --masked_lm_prob=0.15 \
+  --random_seed=12345 \
+  --dupe_factor=5
 
 python3 /crs2/bert/run_pretraining.py \
   --input_file=$BERT_BASE_DIR/tmp/tf_zh_AA.tfrecord \
@@ -28,7 +41,4 @@ python3 run_classifier.py \
   --train_batch_size=16 \
   --learning_rate=5e-5 \
   --num_train_epochs=2.0 \
-  --output_dir=$BERT_BASE_DIR/tmp/xnli_output_msl-256/ \
-  --do_stroke=True \
-  --stroke_vocab_file=$BERT_BASE_DIR/stroke.csv
-
+  --output_dir=$BERT_BASE_DIR/tmp/xnli_output_msl-256/ 
